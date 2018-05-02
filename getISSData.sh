21731bd881fc45a8b784c8ca9cc364b9c2e3b1bd
Niel Mistry <nielmistry1@gmail.com>
2018-05-02 14:02:29 -0400

    Fix auth problem

--- getISSData.sh
+++ getISSData.sh
@@ -1,12 +1,1 @@
-#!/bin/sh
-
-# CURL='/usr/bin/curl'
-curl -c cookies.txt -b cookies.txt https://www.space-track.org/ajaxauth/login -d "identity=nielmistry1@gmail.com&password=oS4c0PiZncxHbo9"
-
-# curlCmd2()
-# {
-# 	curl --limit-rate 100K --cookie cookies.txt https://www.space-track.org/basicspacedata/query/class/tle_latest/OBJECT_ID/1998-067A/orderby/ORDINAL%20asc/limit/1/metadata/false > iss.json
-# }
-
-# curlCmd
-# curlCmd2
+wget  --post-data='identity=nielmistry1@gmail.com&password=oS4c0PiZncxHbo9&query=https://www.space-track.org/basicspacedata/query/class/tle_latest/OBJECT_ID/1998-067A/orderby/ORDINAL%20asc/limit/1/metadata/false' --cookies=on --keep-session-cookies --save-cookies=cookies.txt 'https://www.space-track.org/ajaxauth/login' -O iss.json
