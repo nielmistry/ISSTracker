afe0e0595d3122456e8bca461257b686609b0319
Niel Mistry <nielmistry1@gmail.com>
2018-05-02 21:09:48 -0400

    Add ISS TLE parsing

--- TLE Fetcher/convertor.py
+++ TLE Fetcher/convertor.py
@@ -1,19 +1,25 @@
 from spacetrack import SpaceTrackClient
 import what3words
-import json
-from collections import namedtuple
 
 # What3Words Setup
 THREE_WORDS_API_KEY = "H60K6CTE";
 w3w = what3words.Geocoder(THREE_WORDS_API_KEY);
 
 # SpaceTrack setup
-st = SpaceTrackClient('nielmistry1@gmail.com','oS4c0PiZncxHbo9')
+st = SpaceTrackClient('nielmistry1@gmail.com','oS4c0PiZncxHbo9');
 
-ISS_TLE = st.tle_latest(object_id = '1998-067A', limit = 1, format = 'tle';
+ISS_TLE = st.tle_latest(object_id='1998-067A', limit=1, format='tle');
+i = 0;
+while ISS_TLE[i] != '\n':
+    i = i + 1;
 
+TLE_LINE_1 = ISS_TLE[0:i-1]
+TLE_LINE_2 = ISS_TLE[i+1:]
+print(TLE_LINE_1)
+print(TLE_LINE_2);
 w3wQuery = 'transit.movement.biked';
 
 w3wData = w3w.forward(addr=w3wQuery)
 
-# Convert w3wData to python object
+lat = w3wData['bounds']['southwest']['lat'];
+lng = w3wData['bounds']['southwest']['lng'];
