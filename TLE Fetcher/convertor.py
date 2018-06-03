04f65bb0c5a1f16287ae701b6af0a8cbb2afaf51
Niel Mistry <nielmistry1@gmail.com>
2018-06-03 17:27:15 -0400

    Add SGP4 package to verify checksum

--- TLE Fetcher/convertor.py
+++ TLE Fetcher/convertor.py
@@ -1,24 +1,38 @@
 from spacetrack import SpaceTrackClient
 import what3words
+import ephem
+import sgp4.io
+
 
 # What3Words Setup
-THREE_WORDS_API_KEY = "H60K6CTE";
-w3w = what3words.Geocoder(THREE_WORDS_API_KEY);
+THREE_WORDS_API_KEY = "H60K6CTE"
+w3w = what3words.Geocoder(THREE_WORDS_API_KEY)
 
 # SpaceTrack setup
-st = SpaceTrackClient('nielmistry1@gmail.com','oS4c0PiZncxHbo9');
+st = SpaceTrackClient('nielmistry1@gmail.com','oS4c0PiZncxHbo9')
 
-ISS_TLE = st.tle_latest(object_id='1998-067A', limit=1, format='tle');
-i = 0;
+ISS_TLE = st.tle_latest(object_id='1998-067A', limit=1, format='tle')
+
+i = 0
 while ISS_TLE[i] != '\n':
-    i = i + 1;
+    i += 1
 
 TLE_LINE_1 = ISS_TLE[0:i-1]
-TLE_LINE_2 = ISS_TLE[i+1:]
+TLE_LINE_2 = ISS_TLE[i+1:-1]
 
-w3wQuery = 'transit.movement.biked';
+print(TLE_LINE_1)
+print(TLE_LINE_2)
+
+w3wQuery = 'transit.movement.biked'
 
 w3wData = w3w.forward(addr=w3wQuery)
 
-lat = w3wData['bounds']['southwest']['lat'];
-lng = w3wData['bounds']['southwest']['lng'];
+lat = w3wData['bounds']['southwest']['lat']
+lng = w3wData['bounds']['southwest']['lng']
+
+print(sgp4.io.verify_checksum(TLE_LINE_1))
+# iss = ephem.readtle("ISS (ZARYA)",TLE_LINE_1, TLE_LINE_2)
+# iss.compute("2018/04/20")
+#
+# print(iss.sublong)
+# print(iss.sublat)
