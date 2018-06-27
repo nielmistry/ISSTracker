abfafd7f23eb38899ef122b57d6ea58491e50665
Niel Mistry <nielmistry1@gmail.com>
2018-06-26 21:38:26 -0400

    Add crude ascii sending

--- RPiComm/main.py
+++ RPiComm/main.py
@@ -1,12 +1,31 @@
 import RPi.GPIO as GPIO
 import time
+import binascii
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(04, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
-while True:
-	GPIO.output(04,GPIO.HIGH);
-	time.sleep(0.5)
-	GPIO.output(04,GPIO.LOW);
-	time.sleep(0.5)
+toSend = "Hello, world!"
+sendWord(toSend)
+
+
+def sendWord(word):
+	asciiDecode = int(binascii.hexlify(word))
+	for i in range(0,asciiDecode.bit_length):
+		bit = asciiDecode & 0b01
+		asciiDecode = asciiDecode >> 1
+		if asciiDecode == 1:
+			GPIO.output(4, GPIO.HIGH)
+		else:
+			GPIO.output(4, GPIO.LOW)
+		time.sleep(1)
+
+
+
+def infBlink():
+	while True:
+		GPIO.output(4,GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(4,GPIO.LOW)
+		time.sleep(0.5)
