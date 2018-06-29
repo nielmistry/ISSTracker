e9a7812ba019046139c15a49a81c7da3331cfca1
Niel Mistry <nielmistry1@gmail.com>
2018-06-28 22:04:53 -0400

    big file

--- RPiComm/pi.ino/pi.ino.ino
+++ RPiComm/pi.ino/pi.ino.ino
@@ -3,6 +3,7 @@
 
 bool byteArr[8] = {false};
 int curBit = 0;
+//byte charByte = 0;
 volatile bool bitState = false;
   
 void setup() {
@@ -19,39 +20,49 @@ void loop() {
 
 void printChar()
 {
+  byte charByte = 0;
   for(int i = 0; i < 8; i++)
   {
     if(byteArr[i])
     {
       Serial.print("1");
+//      charByte = charByte | (1 << i);
     }
     else
     {
       Serial.print("0");
     }
   }
+//  Serial.print('\n');
+//  Serial.print(charByte);
 }
 
 void setBitArr(){
+//  if(bitState)
+//  {
+//    charByte |= (1 << curBit);
+//  }
   byteArr[curBit] = bitState;
   curBit++;
   if(curBit == 8)
   {
     curBit = 0;
-//    printChar();
+//    Serial.print(char(charByte)); 
+    printChar();
     Serial.print("\n");
+//    charByte = 0;
   }
  }
 
 void interrupt(){
   if(digitalRead(DATA_PIN) == HIGH)
   {
-    Serial.print("1");
+//    Serial.print("1");
     bitState = true;
   }
   else
   {
-    Serial.print("0");
+//    Serial.print("0");
     bitState = false;
   }
   setBitArr();
